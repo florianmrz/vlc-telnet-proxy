f4dc8cd1e33c76ed0ea1c08dbe9c83fa2683f5f1
Florian Merz <florian.merz@mail.de>
2026-01-31 13:38:58 +0100

    chore: fix __dirname for es6

--- start-service.js
+++ start-service.js
@@ -5,7 +5,7 @@ const svc = new Service({
   name: 'VLC Telnet Proxy',
   description:
     'A Node.js service that synchronizes VLC media player with Home Assistant entity states via telnet commands.',
-  script: path.join(__dirname, 'index.js'),
+  script: path.join(import.meta.dirname, 'index.js'),
   nodeOptions: ['--harmony', '--max_old_space_size=4096'],
   abortOnError: false,
   logmode: 'rotate',
